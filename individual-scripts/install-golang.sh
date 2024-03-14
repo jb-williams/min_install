dfa2ad5387b97546538c02a6dd8055f88cb0beeb
jb <jb.williams016@gmail.com>
2024-03-13 21:38:28 -0500

    updated install-golang.sh

--- individual-scripts/install-golang.sh
+++ individual-scripts/install-golang.sh
@@ -1,36 +1,60 @@
 #!/usr/bin/env bash
-bold='\e[1m'
-red='\e[31m'
-lightyellow='\e[93m'
-blink='\e[5m'
-default='\e[0;39m'
-green='\e[32m'
+# MIT License
+# Copyright (c) 2023 Jb Williams
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-reset2green() {
-    printf "%b%b" "\e[0;39m" "\e[32m"
-}
+# Get Golang Version and the Given Golang Hash for linux
+GO_DLPAGE="$(/usr/bin/curl https://go.dev/dl/ | /usr/bin/grep -iE 'href="/dl/go[0-9].[0-9]{2}.[0-9]{1,2}.linux-amd64.tar.gz"' -m 2 -A 6 | tail -7 | sed -e 's/<[^>]*.//g' | tr '\n' ' ')"
+GO_VERSION="$(printf "%s" "$GO_DLPAGE" | awk '{print $1}')"
+#GO_VERSION="$(echo "$GO_DLPAGE" | awk '{print $1}')"
+DL_HASH="$(printf "%s" "$GO_DLPAGE" | awk '{print $6}')"
 
-error_print() {
-    printf "%b%b%b%s%b%b %s %b%s%b%s\n" "${bold}" "${blink}" "${red}" "ERROR!!!" "${default}" "${green}" "Failed:" "${lightyellow}" "$*" "${green}" "!!!" | tee -a "$HOME"/install-golang-error.log
-}
+# Prompt User, asking if they want to continue
+#echo "Your version of golang is '$(go version 2>/dev/null)'"
+printf "%s!\n" "Your version of golang is '$(go version 2>/dev/null)'"
+read -p "Curl read the current version as $GO_VERSION : Would you like to Continue?(Y/n): " -n 1 -r
+if [[ "$REPLY" =~ ^[Yy]$ ]]; then
+	# Download to  $TEMP dir
+    #pushd /tmp || exit 1
+	cd $TEMP || exit || exit 1
+    /usr/bin/wget https://go.dev/dl/"$GO_VERSION" || exit 1
 
-install_golang() {
-    VERSION="$(curl https://go.dev/dl/ | grep -iE '<span>go[0-9].[0-9]{2}.[0-9]{1,2}.linux-amd64.tar.gz' | sed -e 's/<[^>]*.//g' | tr -d ' ')"
-    read -p "Curl read Current Go Version as \"$VERSION\" : Would you like to Continue?(Y/n): " -n 1 -r
-    if [[ "$REPLY" =~ ^[Yy]$ ]]; then
-        pushd /tmp || error_print "${FUNCNAME[idx]}" 
-        wget https://go.dev/dl/"$VERSION"
-        tar xvfz "$VERSION"
-        if [[ -d /usr/local/go ]]; then
-            sudo sh -c "rm -rf /usr/local/go"
-            sudo sh -c "mv go /usr/local/"
-            rm "$VERSION"
-        else 
-            sudo sh -c "mv go /usr/local/"
-        fi
-        popd || error_print "${FUNCNAME[idx]}" 
-    fi
-}
+	# get sha256 of the file you are wanting to verify
+	if [[ -n "$GO_VERSION" ]] && [[ -f "$GO_VERSION" ]] && [[ ! -d "$GO_VERSION" ]]; then
+		TAR_HASH="$(shasum -a256 "$GO_VERSION" | awk '{print $1}')"
+		[[ "$TAR_HASH" ]] || exit 1
+	else
+		#echo "Unable to find the '.tar.gz', may have been an error downloading!"
+		printf "%s\n" "Unable to find the '.tar.gz', may have been an error downloading!"
+		#popd || exit 1
+		cd "$OLDPWD" || exit 1
+		exit 1
+	fi
 
-reset2green
-install_golang || error_print "install_golang"
+	# Check if the Golang .tar.gz's hash matches the given hash from the download page
+	if [[ "$DL_HASH" == "$TAR_HASH" ]]; then
+		# Decompress and move to proper place removing the older version if it exists
+		printf "%s!\n" "Hashes Match"
+		/usr/bin/tar xfz "$GO_VERSION" || exit 1
+		if [[ -d /usr/local/go ]]; then
+			/usr/bin/sudo rm -rf /usr/local/go || exit 1
+			/usr/bin/sudo mv go /usr/local/ || exit 1
+			/usr/bin/rm "$GO_VERSION" || exit 1
+		else 
+			/usr/bin/sudo mv go /usr/local/ || exit 1
+			/usr/bin/rm "$GO_VERSION" || exit 1
+		fi
+	else
+		# Abort if the .tar.gz's hash doesn't match the given hash from the download page
+		printf "%s!!!!\n" "HASHES DON'T MATCH!!! ABORTING!!"
+		rm "$GO_VERSION" || exit 1
+	fi
+    #popd || return
+	cd "$OLDPWD" || exit 1
+else
+    printf "\n" && exit 0
+fi
+exit 0
+
